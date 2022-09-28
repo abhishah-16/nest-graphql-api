ced75d81d869d9561cdc4b0395c8f51399494b37
abhishah <abhishah0961@gmail.com>
2022-09-28 13:12:57 +0530

    create: donations type

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date'
 
 @Module({
   imports: [GraphQLModule.forRoot({
     playground: false,
     plugins: [ApolloServerPluginLandingPageLocalDefault],
-    typePaths: ['./**/*.graphql']
+    driver: ApolloDriver,
+    typePaths: ['./**/*.graphql'],
+    resolvers: { DateTime: GraphQLDateTime }
   }), DonationsModule],
   controllers: [AppController],
   providers: [AppService],
